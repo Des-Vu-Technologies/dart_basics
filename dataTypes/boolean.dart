832e0d4482c71beeadcc4624fb242bf13844524d
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-09-26 09:00:05 +0545

    bool data type

---
+++ dataTypes/boolean.dart
@@ -0,0 +1,9 @@
+void main() {
+bool isOpen = true;
+print("Dès Vu Technologies is Open today : $isOpen");
+}
+
+/**
+ * boolean holds either true or false value.
+ * we can write the bool keyword to define the boolean data type.
+ */
