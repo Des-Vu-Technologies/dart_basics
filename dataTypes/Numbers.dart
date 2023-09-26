4b9883e2e32df043c678bd2bd01f806d460d2a79
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-09-26 08:34:05 +0545

    Numeric type added

---
+++ dataTypes/Numbers.dart
@@ -0,0 +1,21 @@
+//Data types help you to categorize all the different types of data you use in your code.
+// For e.g. numbers, texts, symbols, etc.
+
+ void main() {
+ // Declaring Variables  
+int number1 = 100; // without decimal .
+double number2 = 130.2; // with decimal .
+num number3 = 50;
+num  number4 = 50.4;  
+
+// For Sum   
+num sum = number1 + number2 + number3 + number4;
+   
+// Printing Info   
+print("Num 1 is $number1");
+print("Num 2 is $number2");  
+print("Num 3 is $number3");  
+print("Num 4 is $number4");  
+print("Sum is $sum");  
+   
+}
---
+++ dataTypes/roundDoubleValueToDecimal.dart
@@ -0,0 +1,10 @@
+void main() {
+// Declaring Variables
+double prize = 1110.22323422231; // valid.
+print(prize.toStringAsFixed(3));
+}
+
+/**
+ * The .toStringAsFixed(3) is used to round the double value upto 3 decimal places in dart. 
+ * We can round to any decimal places by entering numbers like 1, 2, 9, etc.
+ */
