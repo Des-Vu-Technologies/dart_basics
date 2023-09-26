0391d01bf696519e31e02e99bd09377805ded312
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-09-26 08:37:14 +0545

    string in dart

---
+++ dataTypes/String.dart
@@ -0,0 +1,12 @@
+/**
+ * String helps you to store text data. You can store values like I love dart.
+ * we can use single or double quotes to store string in dart.
+ */
+void main() {
+// Declaring variables of type string and assigning values     
+String companyName = "Dès Vu Technologies";
+String address = "Bijayapur, Pokhara-26";   
+
+// Printing Values
+print("Company name is $companyName and address is $address");   
+}
