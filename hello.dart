928187ca2092144fa35de4cd4b9ec5b5343280f2
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-09-26 08:10:45 +0545

    updated hello.md

--- hello.dart
+++ hello.dart
@@ -3,9 +3,3 @@ void main() {
   print("Hello world");
 
 }
-/**
- * void is the return type of main function which is the starting point of any dart programs
- * The curly braces {} represents the begining and end of a block of code
- * print("Hello world") prints the hello world text on the conasole window.
- * Each code statement must end with a semicolon.
- */
